eb33ee97e63b9758cb99fe8a44a5f570b21eb101
eliegaboriau <elie@opensourcepolitics.eu>
2022-02-03 14:17:15 +0100

    montée de version decidim 0.25

--- app/decorators/redirect_rule_decorator.rb
+++ app/decorators/redirect_rule_decorator.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 # This decorator associates the model with a Decidim::Organization and adds a custom AdminLog presenter.
-RedirectRule.class_eval do
-  # Concern added.
-  include Decidim::Traceable
+module RedirectRuleDecorator
+  RedirectRule.class_eval do
+    # Concern added.
+    include Decidim::Traceable
 
-  # Association added.
-  belongs_to :organization, foreign_key: :decidim_organization_id, class_name: "Decidim::Organization"
+    # Association added.
+    belongs_to :organization, foreign_key: :decidim_organization_id, class_name: "Decidim::Organization"
 
-  # Method added.
-  # Sets the presenter class for the :admin_log for a RedirectRule resource.
-  def self.log_presenter_class_for(_log)
-    Decidim::UrlAliases::AdminLog::RedirectRulePresenter
+    # Method added.
+    # Sets the presenter class for the :admin_log for a RedirectRule resource.
+    def self.log_presenter_class_for(_log)
+      Decidim::UrlAliases::AdminLog::RedirectRulePresenter
+    end
   end
 end
+
